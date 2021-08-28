1b9af5d510ae843f7592ed8db75860403a88a6d6
yusei <yusei5884@gmail.com>
2021-08-28 22:26:06 +0900

    first commit

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Custom404: React.FC = () => {
+    return (
+        <main className="main">
+            <p>ページがありません</p>
+        </main>
+    )
+}
+
+export default Custom404;
---
+++ pages/blog/[id].tsx
@@ -0,0 +1,44 @@
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next';
+import React from 'react';
+import { Blog } from '..'
+import { client } from '../../libs/client';
+import styles from '../../styles/Home.module.scss';
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+const BlogId: React.VFC<Props> = ({ blog }) => {
+    return (
+        <main className={styles.main}>
+          <h1 className={styles.title}>{blog.title}</h1>
+          <p className={styles.publishedAt}>{blog.publishedAt}</p>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: `${blog.body}`,
+            }}
+            className={styles.post}
+          />
+        </main>
+      );
+};
+
+export default BlogId;
+
+export const getStaticPaths: GetStaticPaths = async() => {
+    const data: {contents: Blog[] } = await client.get({ endpoint: "blog" });
+    const paths = data.contents.map((content) => `/blog/${content.id}`);
+
+    return {
+        paths, fallback: false
+    };
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+    const id = context.params?.id;
+    const data: Blog[] = await client.get({ endpoint: "blog", contentId: id as string});
+  
+    return {
+      props: {
+        blog: data,
+      },
+    };
+  };
